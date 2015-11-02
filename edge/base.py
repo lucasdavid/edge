6806b2cf1096d48384c252e388a9f3a3c142d324
ldavid <lucasolivdavid@gmail.com>
2015-11-01 23:09:58 -0200

    Add cost base function

---
+++ edge/base.py
@@ -0,0 +1,8 @@
+def cost(g):
+    """Return the cost of a given path or circuit represented by a nx.Graph object.
+
+    :param g: the graph object which represents the path or circuit.
+    :return: the float cost of transversing the path.
+    """
+    return sum((d['weight'] for _, _, d in g.edges(data=True)))
+`
