ebd7e0ad91458f635ba02b7ea06f42c0f7ea8145
ldavid <lucasolivdavid@gmail.com>
2015-11-01 23:11:41 -0200

    Add graphics definition for easier standardized graph printing.

---
+++ edge/graphics.py
@@ -0,0 +1,10 @@
+default_style = {
+    'alpha': .6,
+    'width': 1,
+    'node_size': 100,
+    'node_color': '#2EB1E6',
+    'edge_color': '#cccccc',
+}
+
+solution_style = default_style.copy()
+solution_style.update(node_color='#ff0000')
