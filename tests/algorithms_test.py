317261b387264e96a86a4eab47d1415381b2ad89
ldavid <lucasolivdavid@gmail.com>
2015-11-03 09:58:58 -0200

    Update tests

--- tests/algorithms_test.py
+++ tests/algorithms_test.py
@@ -5,21 +5,21 @@ from edge import algorithms, GraphGenerator
 
 class EdgeScoreTest(TestCase):
     def test_simple_graph(self):
-        g = GraphGenerator(n=4).weighted_graph()
+        g = GraphGenerator(n=4).graph()
         algorithms.EdgeScore(g).solve()
 
     def test_random_graph(self):
-        g = GraphGenerator(n=10, m=30).weighted_graph()
+        g = GraphGenerator(n=10, m=30).graph()
         algorithms.EdgeScore(g).solve()
 
 
 class TwiceAroundTest(TestCase):
     def test_simple_graph(self):
-        g = GraphGenerator(n=4).weighted_graph()
+        g = GraphGenerator(n=4).graph()
         algorithms.TwiceAround(g).solve()
 
 
 class NearestNeighborTest(TestCase):
     def test_simple_graph(self):
-        g = GraphGenerator(n=4).weighted_graph()
+        g = GraphGenerator(n=4).graph()
         algorithms.NearestNeighbor(g).solve()
